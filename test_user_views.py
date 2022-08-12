ffa3ac7cc09fa38d51c6a98d1fde11035ae2163a
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-08-12 09:50:34 -0700

    setup on user view tests

--- test_user_views.py
+++ test_user_views.py
@@ -1,3 +1,57 @@
-# ASK ABOUT ADDING MESSAGES WHILE LOGGED IN AS A DIFFERENT USER??????
+"""User View tests."""
 
-# bring up in stand up
+# run these tests like:
+#
+#    FLASK_ENV=production python -m unittest test_message_views.py
+
+
+import os
+from unittest import TestCase
+
+from models import db, Message, User, LikedMessage
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
+
+from app import app, CURR_USER_KEY
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+db.create_all()
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+
+class UserBaseViewTestCase(TestCase):
+    def setUp(self):
+        User.query.delete()
+
+        u1 = User.signup("u1", "u1@email.com", "password", None)
+        u2 = User.signup("u2", "u2@email.com", "password", None)
+        db.session.flush()
+
+        m1 = Message(text="m1-text", user_id=u1.id)
+        db.session.add_all([m1])
+        db.session.commit()
+
+        lm1 = LikedMessage(user_id = u2.id, message_id=m1.id )
+        db.session.add_all([lm1])
+        db.session.commit()
+
+        self.u1_id = u1.id
+        self.u2_id = u2.id
+        self.m1_id = m1.id
+
+        self.client = app.test_client()
+
+class UserAddViewTestCase(UserBaseViewTestCase):
+    def test_list_users_logged_in(self):
+        """ ! """
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.get("/users", data={"text": "Hello"})
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("", html)
